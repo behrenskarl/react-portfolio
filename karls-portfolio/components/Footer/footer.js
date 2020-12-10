57ad98c176209e7a55fa42796557895b513669bc
DAGDAD <behrens.karl@gmail.com>
2020-12-09 20:21:22 -0800

    added photos to assets folder, initialized components

---
+++ karls-portfolio/components/Footer/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+    return(
+        <div className="footer">  
+            <a href="https://www.facebook.com/karl.behrens" className="fa fa-facebook"></a>
+            <a href="https://www.instagram.com/guitarlkarl/" className="fa fa-instagram"></a>
+            <a href="./assets/Karl Behrens Resume Oct 2020.pdf" className="fa fa-file"></a>
+            <a href="https://github.com/behrenskarl" className="fa fa-github"></a>
+            <a href="https://www.linkedin.com/in/karl-behrens-041ba5125/" className="fa fa-linkedin"></a>
+        </div>
+    );
+}
+
+export default Footer;
